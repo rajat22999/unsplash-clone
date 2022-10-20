3d09c249a491e3de859f15dbcd771450c6ff45d2
Rajat Saraswat <74696478+rajat22999@users.noreply.github.com>
2022-10-20 16:06:51 +0530

    Integrated middleware in store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,6 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import { searchImages } from "../services/imageApi";
 import fetchImage from "./images/fetchImages/fetchImage";
 import randomSlice from "./images/random/randomSlice";
+import thunk from "redux-thunk";
+
 
 const store = configureStore({
   reducer: {
@@ -9,6 +11,7 @@ const store = configureStore({
     random: randomSlice,
     search: searchImages
   },
+  middleware: [thunk],
 });
 
 export default store;
